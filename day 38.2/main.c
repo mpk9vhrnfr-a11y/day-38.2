27aa0eff9b480563205cd80b40bbfe8242b6e847
sanaa <Sanaakumar07@gmail.com>
2025-11-26 13:22:14 +0530

    c

--- day 38.2/main.c
+++ day 38.2/main.c
@@ -1,15 +1,39 @@
-//
-//  main.c
-//  day 38.2
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int a[10][10];
+    int i, j, r, c, flag = 1;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    if(r != c) {
+        printf("Matrix is not symmetric.\n");
+        return 0;
+    }
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 0) break;
+    }
+
+    if(flag)
+        printf("Matrix is symmetric.\n");
+    else
+        printf("Matrix is not symmetric.\n");
+
+    return 0;
 }
+
